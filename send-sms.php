c3bbc5829459ab09e04a6c201588d39da2aa95ac
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-05-15 14:47:16 +0800

    Remove try-catch block

--- send-sms.php
+++ send-sms.php
@@ -4,24 +4,20 @@ require_once 'vendor/autoload.php';
 $to = $_POST['to'];
 $message = $_POST['message'];
 
-const VONAGE_API_KEY = 'YOUR_API_KEY';
-const VONAGE_API_SECRET = 'YOUR_API_SECRET';
+const VONAGE_API_KEY = 'VONAGE_API_KEY';
+const VONAGE_API_SECRET = 'VONAGE_API_SECRET';
 
 $basic  = new \Vonage\Client\Credentials\Basic(VONAGE_API_KEY, VONAGE_API_SECRET);
 $client = new \Vonage\Client($basic);
 
-try {
-    $response = $client->sms()->send(
-        new \Vonage\SMS\Message\SMS($to, 'VONAGE', $message)
-    );
+$response = $client->sms()->send(
+    new \Vonage\SMS\Message\SMS($to, 'VONAGE', $message)
+);
 
-    $messages = $response->current();
+$messages = $response->current();
 
-    if ($messages->getStatus() == 0) {
-        echo "The message was sent successfully\n";
-    } else {
-        echo "The message failed with status: " . $messages->getStatus() . "\n";
-    }
-} catch (Exception $e) {
-    echo "Error: " . $e->getMessage() . "\n";
+if ($messages->getStatus() == 0) {
+    echo "The message was sent successfully\n";
+} else {
+    echo "The message failed with status: " . $messages->getStatus() . "\n";
 }
