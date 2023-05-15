ae7f10545e8967ec08faa249296d9069bcf11c44
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-05-15 15:08:09 +0800

    Add try-catch block

--- send-sms.php
+++ send-sms.php
@@ -10,14 +10,18 @@ const VONAGE_API_SECRET = 'VONAGE_API_SECRET';
 $basic  = new \Vonage\Client\Credentials\Basic(VONAGE_API_KEY, VONAGE_API_SECRET);
 $client = new \Vonage\Client($basic);
 
-$response = $client->sms()->send(
-    new \Vonage\SMS\Message\SMS($to, 'VONAGE', $message)
-);
+try {
+    $response = $client->sms()->send(
+        new \Vonage\SMS\Message\SMS($to, 'VONAGE', $message)
+    );
 
-$messages = $response->current();
+    $messages = $response->current();
 
-if ($messages->getStatus() == 0) {
-    echo "The message was sent successfully\n";
-} else {
-    echo "The message failed with status: " . $messages->getStatus() . "\n";
+    if ($messages->getStatus() == 0) {
+        echo "The message was sent successfully\n";
+    } else {
+        echo "The message failed with status: " . $messages->getStatus() . "\n";
+    }
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage() . "\n";
 }
